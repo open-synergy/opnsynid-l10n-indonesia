7161f30fd0f7dc29529586051498f4e61f14e178
Andhitia Rama <andhitia.r@gmail.com>
2017-03-21 00:31:55 +0700

    8.0 l10n id djbc app (#4)
    * [ADD][8.0] l10n_id_djbc_app
    
    * Fix PEP8
    
    * Change module name

---
+++ l10n_id_djbc_app/models/djbc_custom_document.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class CustomDocument(models.Model):
+    _name = "l10n_id.djbc_custom_document"
+    _description = "Custom Document"
+
+    name = fields.Char(
+        string="# Document",
+        required=True,
+    )
+    date = fields.Date(
+        string="Date",
+        required=True,
+    )
+    type_id = fields.Many2one(
+        string="Type",
+        comodel_name="l10n_id.djbc_document_type",
+        required=True,
+        ondelete="restrict",
+    )
---
+++ l10n_id_djbc_app/models/stock_quant.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    djbc_custom = fields.Boolean(
+        string="DJBC Marking",
+    )
+
+    @api.model
+    def _quant_create(self, qty, move, lot_id=False, owner_id=False,
+                      src_package_id=False, dest_package_id=False,
+                      force_location_from=False, force_location_to=False,
+                      ):
+        quant = super(StockQuant, self)._quant_create(
+            qty, move, lot_id, owner_id, src_package_id, dest_package_id,
+            force_location_from, force_location_to)
+        if move.djbc_custom_document_id:
+            quant.write({"djbc_custom": True})
