e1649f373b11a85e1cb2f1a15655be02f47ecb7b
Andhitia Rama <andhitia.r@gmail.com>
2017-03-21 08:20:46 +0700

    [ADD][8.0] l10n_id_djbc_bc20

---
+++ l10n_id_djbc_bc20/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Form BC 2.0 for DJBC Reporting",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_id_djbc_app",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/djbc_document_type.xml",
+        "views/djbc_bc20_views.xml",
+    ],
+}
