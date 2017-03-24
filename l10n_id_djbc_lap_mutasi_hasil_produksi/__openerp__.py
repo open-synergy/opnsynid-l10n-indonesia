08df0f57c1c9d8595166b2572990b08ca7e8b44f
Andhitia Rama <andhitia.r@gmail.com>
2017-03-24 22:25:39 +0700

    [ADD][8.0] l10n_id_djbc_lap_mutasi_hasil_produksi

---
+++ l10n_id_djbc_lap_mutasi_hasil_produksi/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Lap. Mutasi Hasil Produksi for DJBC Reporting",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_id_djbc_lap_mutasi_helper",
+    ],
+    "data": [
+        "reports/lap_mutasi_hasil_produksi_views.xml",
+    ],
+}
