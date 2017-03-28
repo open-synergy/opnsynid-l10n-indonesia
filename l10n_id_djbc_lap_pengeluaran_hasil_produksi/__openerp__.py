99a0be2e163affd5b6a6dbdbc4586ba456df0130
Andhitia Rama <andhitia.r@gmail.com>
2017-03-28 16:02:27 +0700

    [ADD][8.0] l10n_id_djbc_lap_pengeluaran_hasil_produksi

---
+++ l10n_id_djbc_lap_pengeluaran_hasil_produksi/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Lap. Pengeluaran Hasil Produksi for DJBC Reporting",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_id_djbc_bc30",
+        "stock_transport_multi_address",
+        "sale_stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_picking_type_views.xml",
+        "reports/lap_pengeluaran_hasil_produksi_views.xml",
+    ],
+}
