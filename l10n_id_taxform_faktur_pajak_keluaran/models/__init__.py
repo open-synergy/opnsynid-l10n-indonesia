3a1a539602245ad47c18cf299402c6706c957cce
Andhitia Rama <andhitia.r@gmail.com>
2017-07-09 21:19:30 +0700

    [8.0.1.0.0][l10n_id_taxform_faktur_pajak_keluaran]

---
+++ l10n_id_taxform_faktur_pajak_keluaran/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import nomor_seri_faktur_pajak
+from . import faktur_pajak_keluaran
