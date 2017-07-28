ba5189fd61f4c58d581454af6186d9743b4c6921
Andhitia Rama <andhitia.r@gmail.com>
2017-07-28 15:09:15 +0700

    [8.0.1.0.0][l10n_id_pph_23_tax_data]

---
+++ l10n_id_pph_23_tax_data/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - PPh 23 Tax Data",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "l10n_id_chart_of_account_template",
+    ],
+    "data": [
+        "data/account_tax_code_data.xml",
+        "data/account_tax_data.xml",
+    ],
+}
