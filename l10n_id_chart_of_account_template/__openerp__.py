ed86827df43c6dec137a3cccdc6a866aa76b1055
Andhitia Rama <andhitia.r@gmail.com>
2017-07-28 14:20:28 +0700

    [8.0.1.0.0][l10n_id_chart_of_account_template]

---
+++ l10n_id_chart_of_account_template/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Indonesia - Chart of Account Template",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "data/account_chart_template_data.xml",
+    ],
+}
