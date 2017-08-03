433f8e5f425a4cda24dcc2bcc06ad51d9d30fabb
Andhitia Rama <andhitia.r@gmail.com>
2017-08-03 15:54:51 +0700

    [8.0.1.0.0][l10n_id_taxform_bukti_potong_pph_f113311]

---
+++ l10n_id_taxform_bukti_potong_pph_f113311/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import bukti_potong_pph_f113311_in
+from . import bukti_potong_pph_f113311_out
