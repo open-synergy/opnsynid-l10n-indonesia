5ac70ef0ba2608c61717ae31282959fe565a6a6f
Andhitia Rama <andhitia.r@gmail.com>
2017-08-03 16:20:50 +0700

    [8.0.1.1.0][l10n_id_taxform_bukti_potong_pph_f113311]

--- l10n_id_taxform_bukti_potong_pph_f113311/__openerp__.py
+++ l10n_id_taxform_bukti_potong_pph_f113311/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Bukti Potong PPh 4 Ayat 2 (f.1.1.33.11)",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
