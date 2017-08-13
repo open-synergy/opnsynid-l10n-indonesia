314e22922953b5e7d0854dfe2634311ba4b08fa5
Andhitia Rama <andhitia.r@gmail.com>
2017-08-13 15:54:47 +0700

    [8.0.1.1.0][l10n_id_account_type]

--- l10n_id_account_type/__openerp__.py
+++ l10n_id_account_type/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Account Type",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "category": "Localization",
