a8e6c0c535323b6ed2ba14113512c15fb76667f3
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-21 18:45:18 +0700

    [8.0.1.1.1]Removing wizard date_range_selector

--- l10n_id_djbc_kb_lap_common/__openerp__.py
+++ l10n_id_djbc_kb_lap_common/__openerp__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 OpenSynergy Indonesia
+# Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Common Feature for Kawasan Berikat Reporting",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.1",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -16,8 +16,6 @@
     "data": [
         "data/product_category_data.xml",
         "menu.xml",
-        "wizards/date_range_selector_views.xml",
-        "reports/lap_kb_mutasi_common_views.xml",
         "views/stock_picking_type_views.xml",
     ],
 }
