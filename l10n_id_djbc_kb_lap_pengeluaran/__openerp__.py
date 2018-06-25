5351438cf90801b970818051abb52ec16f45e720
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-25 16:00:14 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_pengeluaran]

--- l10n_id_djbc_kb_lap_pengeluaran/__openerp__.py
+++ l10n_id_djbc_kb_lap_pengeluaran/__openerp__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 OpenSynergy Indonesia
+# Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Indonesia - Lap.  Pengeluaran for DJBC's Kawasan Berikat Reporting",
+    "name": "Indonesia - Lap. Pengeluaran for DJBC's Kawasan Berikat Reporting",
     "version": "8.0.1.0.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
@@ -10,9 +10,10 @@
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "l10n_id_djbc_kb_lap_common",
+        "l10n_id_djbc_kb_lap_common"
     ],
     "data": [
         "wizards/date_range_selector_views.xml",
+        "reports/lap_kb_lap_pengeluaran.xml"
     ],
 }
