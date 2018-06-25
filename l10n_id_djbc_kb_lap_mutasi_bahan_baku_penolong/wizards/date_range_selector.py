c26b917ed29fe3d6f8ea6b17419b7aba83a4b397
mikevhe18 <viriyananda.michael@gmail.com>
2018-06-25 16:55:25 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong] Fixing Travis Error

--- l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong/wizards/date_range_selector.py
+++ l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong/wizards/date_range_selector.py
@@ -2,10 +2,7 @@
 # Copyright 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from datetime import datetime
-from openerp import api, models, fields
-from openerp.tools.translate import _
-from openerp.exceptions import Warning as UserError
+from openerp import api, models
 
 
 class DateRangeSelector(models.TransientModel):
@@ -15,13 +12,12 @@ class DateRangeSelector(models.TransientModel):
     def action_mutasi_bahan_baku_penolong(self):
         self.ensure_one()
         waction = self.env.ref(
-            "l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong.djbc_kb_lap_mutasi_bahan_baku_penolong_action")
+            "l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong."
+            "djbc_kb_lap_mutasi_bahan_baku_penolong_action")
         result = waction.read()[0]
         context = {
             "date_start": self.date_start,
             "date_end": self.date_end,
-            }
+        }
         result.update({"context": context})
         return result
-
-
