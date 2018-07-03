3f9bcb67e1faba9e43653456281d5308650668a5
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-03 13:23:10 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_mutasi_reject_scrap]

---
+++ l10n_id_djbc_kb_lap_mutasi_reject_scrap/reports/lap_kb_mutasi_reject_scrap.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+from openerp import tools
+
+
+class LapKbMutasiRejectScrap(models.Model):
+    _name = "l10n_id.djbc_kb_lap_mutasi_reject_scrap"
+    _inherit = "l10n_id.djbc_kb_lap_mutasi_common"
+    _description = "Laporan Mutasi Reject/Scrap Untuk Kawasan Berikat"
+    _auto = False
+
+    def _join(self):
+        _super = super(LapKbMutasiRejectScrap, self)
+        join_str = _super._join() + """
+        JOIN product_categ_rel AS c ON
+            b.id = c.product_id
+        JOIN product_category AS d ON
+            c.categ_id = d.id
+        JOIN (
+            SELECT res_id
+            FROM ir_model_data AS e1
+            WHERE 
+                e1.module = 'l10n_id_djbc_kb_lap_common' AND
+                e1.name = 'product_categ_kb_scrap'
+            ) as e ON
+            d.id = e.res_id
+        """
+        return join_str
