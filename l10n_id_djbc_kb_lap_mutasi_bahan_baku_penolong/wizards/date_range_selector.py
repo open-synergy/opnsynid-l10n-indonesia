bc4fb60a9f1302c6bfad3698bea27fe299b775e8
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-03 13:26:31 +0700

    [8.0.1.1.0][l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong] * Re-define warehouse_ids

--- l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong/wizards/date_range_selector.py
+++ l10n_id_djbc_kb_lap_mutasi_bahan_baku_penolong/wizards/date_range_selector.py
@@ -2,12 +2,20 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from openerp import api, models, fields
 
 
 class KBLapMutasiBahanBakuWizard(models.TransientModel):
     _name = "l10n_id.kb_lap_mutasi_bahan_baku_wizard"
     _inherit = ["l10n_id.date_range_selector"]
+
+    warehouse_ids = fields.Many2many(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        relation="rel_djbc_bahan_baku_2_warehouse",
+        column1="wizard_id",
+        column2="warehouse_id"
+    )
 
     @api.multi
     def action_print_sreen(self):
