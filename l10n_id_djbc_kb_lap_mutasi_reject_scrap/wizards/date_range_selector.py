7e2f635fe3a348f00cf6a21210daaf826108626d
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-03 13:23:10 +0700

    [8.0.1.0.0][l10n_id_djbc_kb_lap_mutasi_reject_scrap]

---
+++ l10n_id_djbc_kb_lap_mutasi_reject_scrap/wizards/date_range_selector.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class KBLapMutasiRejectScrapWizard(models.TransientModel):
+    _name = "l10n_id.kb_lap_mutasi_reject_scrap_wizard"
+    _inherit = ["l10n_id.date_range_selector"]
+
+    warehouse_ids = fields.Many2many(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        relation="rel_djbc_reject_scrap_2_warehouse",
+        column1="wizard_id",
+        column2="warehouse_id"
+    )
+
+    @api.multi
+    def action_print_sreen(self):
+        waction = self.env.ref(
+            "l10n_id_djbc_kb_lap_mutasi_reject_scrap."
+            "djbc_kb_lap_mutasi_reject_scrap_action")
+        return waction.read()[0]
