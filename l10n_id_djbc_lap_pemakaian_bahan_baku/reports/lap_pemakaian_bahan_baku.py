c6989464d8d29c749e18da0db64b861e3edae86f
Andhitia Rama <andhitia.r@gmail.com>
2018-07-16 19:48:46 +0700

    [LINT] l10n_id_djbc_lap_pemakaian_bahan_baku

--- l10n_id_djbc_lap_pemakaian_bahan_baku/reports/lap_pemakaian_bahan_baku.py
+++ l10n_id_djbc_lap_pemakaian_bahan_baku/reports/lap_pemakaian_bahan_baku.py
@@ -96,6 +96,7 @@ class LapPemakaianBahanBaku(models.Model):
 
     def init(self, cr):
         tools.drop_view_if_exists(cr, self._table)
+        # pylint: disable=locally-disabled, sql-injection
         cr.execute("""CREATE or REPLACE VIEW %s as (
             %s
             FROM ( %s )
