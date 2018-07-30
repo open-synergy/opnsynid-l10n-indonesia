0a98e0ad4336869b34387719984c3ed7b2850fc7
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-30 18:50:27 +0700

    [8.0.1.0.0][l10n_id_djbc_bc40]

---
+++ l10n_id_djbc_bc40/models/djbc_bc40.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class DjbcForm40(models.Model):
+    _name = "l10n_id.djbc_bc40"
+    _description = "Form 4.0 DJBC"
+    _inherits = {
+        "l10n_id.djbc_custom_document": "custom_document_id",
+    }
+
+    custom_document_id = fields.Many2one(
+        string="Custom Document",
+        comodel_name="l10n_id.djbc_custom_document",
+        required=True,
+        ondelete="restrict",
+    )
