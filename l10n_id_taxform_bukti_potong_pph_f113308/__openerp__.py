78ceca33f6da71d866aae7627aaa0c3695dd5b00
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-31 15:47:27 +0700

    [8.0.1.1.0][l10n_id_taxform_bukti_potong_pph_f113308]

--- l10n_id_taxform_bukti_potong_pph_f113308/__openerp__.py
+++ l10n_id_taxform_bukti_potong_pph_f113308/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Bukti Potong PPh Pasal 26 (F.1.1.33.08)",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
--- l10n_id_taxform_bukti_potong_pph_f113308/models/bukti_potong_pph_f113308_in.py
+++ l10n_id_taxform_bukti_potong_pph_f113308/models/bukti_potong_pph_f113308_in.py
@@ -22,10 +22,11 @@ class BuktiPotongPPhF113308In(models.Model):
     )
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
         type_id = self.env.ref(
             "l10n_id_taxform_bukti_potong_pph_f113308."
             "bukti_potong_pph_type_f113308_in")
         args.append(("type_id", "=", type_id.id))
-        return super(BuktiPotongPPhF113308In, self).\
-            search(args, offset, limit, order)
+        return super(BuktiPotongPPhF113308In, self).search(
+            args=args, offset=offset, limit=limit,
+            order=order, count=count)
