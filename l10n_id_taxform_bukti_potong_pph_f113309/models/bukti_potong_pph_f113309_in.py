f15675c6143281455766fe50d310b999f227ac7e
mikevhe18 <viriyananda.michael@gmail.com>
2018-07-31 15:48:03 +0700

    [8.0.1.1.0][l10n_id_taxform_bukti_potong_pph_f113309]

--- l10n_id_taxform_bukti_potong_pph_f113309/models/bukti_potong_pph_f113309_in.py
+++ l10n_id_taxform_bukti_potong_pph_f113309/models/bukti_potong_pph_f113309_in.py
@@ -22,10 +22,11 @@ class BuktiPotongPPhF113309In(models.Model):
     )
 
     @api.model
-    def search(self, args, offset=0, limit=None, order=None):
+    def search(self, args, offset=0, limit=None, order=None, count=False):
         type_id = self.env.ref(
             "l10n_id_taxform_bukti_potong_pph_f113309."
             "bukti_potong_pph_type_f113309_in")
         args.append(("type_id", "=", type_id.id))
-        return super(BuktiPotongPPhF113309In, self).\
-            search(args, offset, limit, order)
+        return super(BuktiPotongPPhF113309In, self).search(
+            args=args, offset=offset, limit=limit,
+            order=order, count=count)
