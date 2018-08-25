58d7a8373eaa0af05b4d5a6745ac82e2c8d8a7b6
Andhitia Rama <andhitia.r@gmail.com>
2018-08-25 17:41:51 +0700

    [8.0.1.4.0] l10n_id_djbc_kb_lap_common

--- l10n_id_djbc_kb_lap_common/__openerp__.py
+++ l10n_id_djbc_kb_lap_common/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Common Feature for Kawasan Berikat Reporting",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.4.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
@@ -12,6 +12,7 @@
     "depends": [
         "l10n_id_djbc_app",
         "product_m2mcategories",
+        "stock_adjustment_operation",
     ],
     "data": [
         "data/product_category_data.xml",
@@ -19,5 +20,6 @@
         "reports/lap_kb_mutasi_common_views.xml",
         "views/stock_picking_type_views.xml",
         "views/stock_inventory_views.xml",
+        "views/res_company_views.xml",
     ],
 }
--- l10n_id_djbc_kb_lap_common/models/__init__.py
+++ l10n_id_djbc_kb_lap_common/models/__init__.py
@@ -2,5 +2,6 @@
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import res_company
 from . import stock_picking_type
 from . import stock_inventory
