ddfb52ebdc7c8dc8ec5e9fd4927139fb306448ad
Andhitia Rama <andhitia.r@gmail.com>
2018-09-16 15:30:49 +0700

    [8.0.1.0.0] l10n_id_ppajp_daftar_klien_non_audit_umum

---
+++ l10n_id_ppajp_daftar_klien_non_audit_umum/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - PPAJP - Daftar Klien Non Audit Umum",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "depends": [
+        "accountant_report",
+        "l10n_id_ppajp_kap",
+        "partner_sector"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "reports/daftar_klien_non_audit_umum.xml"
+    ],
+}
