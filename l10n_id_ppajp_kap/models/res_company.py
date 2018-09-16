807bf60e08469687e3a0e49de8f019b946ba09dd
Andhitia Rama <andhitia.r@gmail.com>
2018-09-16 10:08:22 +0700

    [8.0.1.0.0] l10n_id_ppajp_kap

---
+++ l10n_id_ppajp_kap/models/res_company.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    jasa_audit_umum_ids = fields.Many2many(
+        string="Jasa Audit Umum",
+        comodel_name="accountant.service",
+        relation="rel_company_2_jasa_audit_umum",
+        column1="company_id",
+        column2="service_id",
+    )
+
+    jasa_non_audit_umum_ids = fields.Many2many(
+        string="Jasa Non-Audit Umum",
+        comodel_name="accountant.service",
+        relation="rel_company_2_jasa_non_audit_umum",
+        column1="company_id",
+        column2="service_id",
+    )
