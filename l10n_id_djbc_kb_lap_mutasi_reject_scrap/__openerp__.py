66d165ea59ea91601ffb2010dab86706ba003504
Andhitia Rama <andhitia.r@gmail.com>
2018-09-16 10:36:04 +0700

    [LINT] l10n_id_djbc_kb_lap_mutasi_reject_scrap

--- l10n_id_djbc_kb_lap_mutasi_reject_scrap/__openerp__.py
+++ l10n_id_djbc_kb_lap_mutasi_reject_scrap/__openerp__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Indonesia - Lap.  Mutasi Reject Scrap "
             "for DJBC's Kawasan Berikat Reporting",
