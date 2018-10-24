5be1babed6eda3440351c26e56c13cf77555559a
mikevhe18 <viriyananda.michael@gmail.com>
2018-10-24 13:24:33 +0700

    [8.0.1.0.0][l10n_id_djbc_plb_lap_pemasukan]

---
+++ l10n_id_djbc_plb_lap_pemasukan/wizards/date_range_selector.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class PLBLapPemasukanWizard(models.TransientModel):
+    _name = "l10n_id.plb_lap_pemasukan_wizard"
+    _inherit = ["l10n_id.date_range_selector"]
+
+    warehouse_ids = fields.Many2many(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        relation="rel_plb_lap_pemasukan_2_warehouse",
+        column1="wizard_id",
+        column2="warehouse_id"
+    )
+
+    @api.multi
+    def action_print_sreen(self):
+        waction = self.env.ref(
+            "l10n_id_djbc_plb_lap_pemasukan.djbc_plb_lap_pemasukan_action")
+        criteria = [
+            ("tgl_penerimaan", ">=", self.date_start),
+            ("tgl_penerimaan", "<=", self.date_end),
+            ("warehouse_id", "in", self.warehouse_ids.ids)
+        ]
+        waction.domain = criteria
+        return waction.read()[0]
