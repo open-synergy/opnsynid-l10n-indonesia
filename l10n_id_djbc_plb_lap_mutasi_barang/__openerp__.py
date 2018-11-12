a70ce0c02d8aaec0fe0c48088c52d2b125596335
mikevhe18 <viriyananda.michael@gmail.com>
2018-11-12 17:15:44 +0700

    [8.0.1.0.0][l10n_id_djbc_plb_lap_mutasi_barang]

---
+++ l10n_id_djbc_plb_lap_mutasi_barang/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Indonesia - Lap. Mutasi Barang for DJBC's "
+            "Pusat Logistik Berikat",
+    "version": "8.0.1.0.0",
+    "category": "localization",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "l10n_id_djbc_plb_common"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizards/date_range_selector_views.xml",
+        "reports/plb_lap_mutasi_barang.xml"
+    ],
+}
---
+++ l10n_id_djbc_plb_lap_mutasi_barang/wizards/date_range_selector.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class PLBLapMutasiBarangWizard(models.TransientModel):
+    _name = "l10n_id.plb_lap_mutasi_barang_wizard"
+    _inherit = ["l10n_id.date_range_selector"]
+
+    warehouse_ids = fields.Many2many(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        relation="rel_plb_lap_mutasi_barang_2_warehouse",
+        column1="wizard_id",
+        column2="warehouse_id"
+    )
+
+    @api.multi
+    def action_print_sreen(self):
+        waction = self.env.ref(
+            "l10n_id_djbc_plb_lap_mutasi_barang.djbc_plb_lap_mutasi_barang_action")
+        context = {
+            "date_start": self.date_start,
+            "date_end": self.date_end,
+        }
+        domain = [("warehouse_id", "in", self.warehouse_ids.ids)]
+        result = waction.read()[0]
+        result.update({"context": context, "domain": domain})
+        return result
