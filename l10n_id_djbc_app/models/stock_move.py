06c54c29d944e6a1077047c0c5c04590afe6edce
Andhitia Rama <andhitia.r@gmail.com>
2018-12-06 13:57:52 +0700

    [8.0.1.6.0] l10n_id_djbc_app

--- l10n_id_djbc_app/models/stock_move.py
+++ l10n_id_djbc_app/models/stock_move.py
@@ -21,6 +21,21 @@ class StockMove(models.Model):
                 if not quant.djbc_custom:
                     move.djbc_custom = False
 
+    @api.depends(
+        "picking_type_id",
+        "picking_type_id.allowed_document_type_ids")
+    def _compute_all_allowed_document_type_ids(self):
+        obj_document_type =\
+            self.env["l10n_id.djbc_document_type"]
+        for move in self:
+            if move.picking_type_id and \
+                    move.picking_type_id.allowed_document_type_ids:
+                move.all_allowed_document_type_ids = \
+                    move.picking_type_id.allowed_document_type_ids
+            else:
+                move.all_allowed_document_type_ids = \
+                    obj_document_type.search([])
+
     djbc_custom_document_id = fields.Many2one(
         string="DJBC Custom Document",
         comodel_name="l10n_id.djbc_custom_document",
@@ -31,4 +46,10 @@ class StockMove(models.Model):
         compute="_compute_djbc",
         store=True,
         readonly=True,
+    )
+    all_allowed_document_type_ids = fields.Many2many(
+        string="All Allowed Document Type",
+        comodel_name="l10n_id.djbc_document_type",
+        compute="_compute_all_allowed_document_type_ids",
+        store=False,
     )
