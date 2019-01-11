430d431ed63a0b61264f1db1011ac5b1c62e1d60
Andhitia Rama <andhitia.r@gmail.com>
2019-01-11 11:41:37 +0700

    [8.0.1.7.0] l10n_id_djbc_kb_lap_common

--- l10n_id_djbc_kb_lap_common/__openerp__.py
+++ l10n_id_djbc_kb_lap_common/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Indonesia - Common Feature for Kawasan Berikat Reporting",
-    "version": "8.0.1.6.0",
+    "version": "8.0.1.7.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
