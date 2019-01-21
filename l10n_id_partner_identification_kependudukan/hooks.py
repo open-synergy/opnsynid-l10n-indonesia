06dcea3b9b5339094da3ba41e1b77966079451a1
Andhitia Rama <andhitia.r@gmail.com>
2019-01-21 17:57:40 +0700

    [8.0.1.0.0] l10n_id_partner_identification_kependudukan

---
+++ l10n_id_partner_identification_kependudukan/hooks.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def _load_partner_identification_data(cr):
+    try:
+        from openupgradelib import load_data
+        load_data(
+            cr, "l10n_id_partner_identification_kependudukan",
+            "/data/res_partner_id_category_data.xml")
+    except:
+        return
