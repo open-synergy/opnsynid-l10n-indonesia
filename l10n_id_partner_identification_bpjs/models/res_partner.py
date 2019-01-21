ec67e21ad4caa61b7375456a4ecaf97662ee2e72
Andhitia Rama <andhitia.r@gmail.com>
2019-01-21 16:44:54 +0700

    [8.0.1.0.0] l10n_id_partner_identification_bpjs

---
+++ l10n_id_partner_identification_bpjs/models/res_partner.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    bpjs_kesehatan_number = fields.Char(
+        string="# BPJS Kesehatan",
+        compute=lambda s: s._compute_identification(
+            "bpjs_kesehatan_number", "bpjs_kes"),
+        search=lambda s, *a: s._search_identification(
+            "bpjs_kes", *a),
+    )
+    bpjs_ketenagakerjaan_number = fields.Char(
+        string="# BPJS Ketenagakerjaan",
+        compute=lambda s: s._compute_identification(
+            "bpjs_ketenagakerjaan_number", "bpjs_tnk"),
+        search=lambda s, *a: s._search_identification(
+            "bpjs_tnk", *a),
+    )
