bcc2ff9ce85402cfed40a616ba64d0e3b75d4be2
Andhitia Rama <andhitia.r@gmail.com>
2019-01-29 09:34:07 +0700

    [8.0.1.3.0] l10n_id_djbc_plb_lap_pengeluaran
    * Get field nilai from invoice value, not from quant
    * Refactor

--- l10n_id_djbc_plb_lap_pengeluaran/__openerp__.py
+++ l10n_id_djbc_plb_lap_pengeluaran/__openerp__.py
@@ -5,14 +5,15 @@
 {
     "name": "Indonesia - Lap. Pengeluaran for DJBC's "
             "Pusat Logistik Berikat",
-    "version": "8.0.1.2.0",
+    "version": "8.0.1.3.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
-        "l10n_id_djbc_plb_common"
+        "l10n_id_djbc_plb_common",
+        "stock_picking_invoice_link",
     ],
     "data": [
         "security/ir.model.access.csv",
