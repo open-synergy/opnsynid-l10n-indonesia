d0000da73757e0a46391c5f7b825716a9aff72ac
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-11 18:16:22 +0700

    [8.0.1.0.0][l10n_id_djbc_kite_common]
    [8.0.1.0.0] Adding ACL for Movement Type Object

---
+++ l10n_id_djbc_kite_common/models/stock_picking_type.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    djbc_kite_movement_type_id = fields.Many2one(
+        string="DJBC's Kite Movement Type",
+        comodel_name="l10n_id.djbc_kite_movement_type"
+    )
+    djbc_kite_scrap = fields.Boolean(
+        string="DJBC's KITE Scrap?",
+        default=False,
+    )
