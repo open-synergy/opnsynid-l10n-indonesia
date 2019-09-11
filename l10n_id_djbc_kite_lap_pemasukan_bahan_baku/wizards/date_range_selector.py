40dea965f82f25e9baabc7009ebb2e09bf4825c4
mikevhe18 <viriyananda.michael@gmail.com>
2019-09-11 18:17:39 +0700

    [8.0.1.0.0][l10n_id_djbc_kite_lap_pemasukan_bahan_baku]
    [8.0.1.0.0]Fixing Travis Error

---
+++ l10n_id_djbc_kite_lap_pemasukan_bahan_baku/wizards/date_range_selector.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import api, models, fields
+
+
+class KitePemasukanBahanBakuWizard(models.TransientModel):
+    _name = "l10n_id.kite_pemasukan_bahan_baku_wizard"
+    _inherit = [
+        "l10n_id.date_range_selector"
+    ]
+
+    warehouse_ids = fields.Many2many(
+        string="Warehouse",
+        comodel_name="stock.warehouse",
+        relation="rel_kite_pemasukan_baku_2_warehouse",
+        column1="wizard_id",
+        column2="warehouse_id"
+    )
+
+    @api.multi
+    def action_print_sreen(self):
+        waction = self.env.ref(
+            "l10n_id_djbc_kite_lap_pemasukan_bahan_baku."
+            "lap_kite_pemasukan_bahan_baku_action")
+        criteria = [
+            ("tgl_penerimaan", ">=", self.date_start),
+            ("tgl_penerimaan", "<=", self.date_end),
+            ("gudang", "in", self.warehouse_ids.ids)
+        ]
+        waction.domain = criteria
+        return waction.read()[0]
