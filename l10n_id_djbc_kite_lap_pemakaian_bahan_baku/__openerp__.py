ef461b0ac7999362f3f7a623d07fc8bdbc7c624e
mikevhe18 <viriyananda.michael@gmail.com>
2019-10-15 13:03:35 +0700

    [8.0.1.2.0][l10n_id_djbc_kite_lap_pemakaian_bahan_baku]

--- l10n_id_djbc_kite_lap_pemakaian_bahan_baku/__openerp__.py
+++ l10n_id_djbc_kite_lap_pemakaian_bahan_baku/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Indonesia - Laporan Pemakaian Bahan Baku for KITE",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "category": "localization",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
