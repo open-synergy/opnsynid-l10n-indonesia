400cabc8e8a1549d87ce2dba5b9f21d6b03ecd70
Andhitia Rama <andhitia.r@gmail.com>
2020-09-24 06:13:12 +0700

    [l10n_id_djbc_plb_lap_pengeluaran]
    * Only get transferred stock.move
    * Add so value
    * Add simetri sinergi branding

--- l10n_id_djbc_plb_lap_pengeluaran/__openerp__.py
+++ l10n_id_djbc_plb_lap_pengeluaran/__openerp__.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Indonesia - Lap. Pengeluaran for DJBC's "
             "Pusat Logistik Berikat",
-    "version": "8.0.1.3.0",
+    "version": "8.0.2.0.0",
     "category": "localization",
-    "website": "https://opensynergy-indonesia.com",
-    "author": "OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia,OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
     "depends": [
         "l10n_id_djbc_plb_common",
         "stock_picking_invoice_link",
+        "sale_stock",
     ],
     "data": [
         "security/ir.model.access.csv",
